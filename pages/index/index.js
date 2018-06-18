b8b6583d910ea4bc75d2df46daa6d580fc54b1bd
sword30021 <sword30021@gmail.com>
2018-06-18 22:58:09 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        itemInfo: {
+            imageLeft: '../../resources/imgs/ic_setting.png',
+            labelCenter: '设置',
+            imageRight: '../../resources/imgs/ic_arrow_right.png'
+        }
+    },
+
+    onLoad: function() {
+
+    },
+
+})
